65702209515abbb29314eb40b1e5f1cfa6eb394b
gaeunnlee <gaeunnnleee@gmail.com>
2025-12-14 22:57:02 +0900

    feat: 비밀번호 해시 및 UUID 변환 함수 구현

---
+++ websocket-jsp/src/main/java/util/UuidRaw.java
@@ -0,0 +1,20 @@
+package util;
+
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
+public class UuidRaw {
+    public static byte[] uuidToRaw(UUID uuid) {
+        ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
+        bb.putLong(uuid.getMostSignificantBits());
+        bb.putLong(uuid.getLeastSignificantBits());
+        return bb.array();
+    }
+
+    public static UUID rawToUuid(byte[] raw) {
+        ByteBuffer bb = ByteBuffer.wrap(raw);
+        long high = bb.getLong();
+        long low = bb.getLong();
+        return new UUID(high, low);
+    }
+}
